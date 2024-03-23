da7b4f85b795bd6f2ec6cdf27cfb3c7b44629ca0
Pepo <Pepo@example.com>
2024-03-23 19:52:56 +0200

    apolix code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { Section2Component } from './section2/section2.component';
+import { HomeComponent } from './home/home.component';
+import { Section3Component } from './section3/section3.component';
+import { Section4Component } from './section4/section4.component';
+import { Section5Component } from './section5/section5.component';
+import { Section6Component } from './section6/section6.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    Section2Component,
+    HomeComponent,
+    Section3Component,
+    Section4Component,
+    Section5Component,
+    Section6Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/section3/section3.component.ts
@@ -0,0 +1,13 @@
+import { style } from '@angular/animations';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section3',
+  templateUrl: './section3.component.html',
+  styleUrls: ['./section3.component.css']
+})
+export class Section3Component {
+
+}
+
+
